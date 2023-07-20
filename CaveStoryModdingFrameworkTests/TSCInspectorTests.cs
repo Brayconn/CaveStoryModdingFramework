80974726e3d5fa81e8459dc5beb75a984697ef69
Brayconn <29030756-Brayconn@users.noreply.github.com>
2023-07-19 23:18:06 -0700

    Extracted some tests into their own files

---
+++ CaveStoryModdingFrameworkTests/TSCInspectorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit;
+using CaveStoryModdingFramework.Editors;
+
+namespace CaveStoryModdingFrameworkTests
+{
+    public class TSCInspectorTests
+    {
+        private readonly ITestOutputHelper output;
+        public TSCInspectorTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        //TODO visually inspecting the output from this is pretty bad...
+        [Theory]
+        [InlineData("\0\0\0\0")]
+        [InlineData("\x7F\x7F\x7F\x7F")]
+        [InlineData("\x80\x80\x80\x80")]
+        [InlineData("\xFF\xFF\xFF\xFF")]
+        [InlineData("0000")]
+        [InlineData("0001")]
+        [InlineData("0200")]
+        [InlineData("000/")]
+        [InlineData("00/:")]
+        [InlineData("001&")]
+        public void InspectorWorks(string data)
+        {
+            var i = new TSCInspector(Encoding.Latin1.GetBytes(data));
+            output.WriteLine(i.ToString());
+        }
+    }
+}
---
+++ CaveStoryModdingFrameworkTests/TSCNumberTests.cs
@@ -0,0 +1,137 @@
+﻿using CaveStoryModdingFramework.TSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CaveStoryModdingFrameworkTests
+{
+    public class TSCNumberTests
+    {
+        private readonly ITestOutputHelper output;
+        public TSCNumberTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void TSC2IteratorRangeOk()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => FlagConverter.Iterate2Flags(FlagConverter.MIN_2_DIGIT - 1).ToArray());
+            Assert.Throws<ArgumentOutOfRangeException>(() => FlagConverter.Iterate2Flags(FlagConverter.MAX_2_DIGIT + 1).ToArray());
+        }
+
+        /// <summary>
+        /// Uses brute force to check that the function works
+        /// </summary>
+        [Fact]
+        public void TSC2IteratorWorks()
+        {
+            var h = new Dictionary<int, HashSet<(int, int)>>();
+            for (int i = FlagConverter.MIN_DIGIT; i <= FlagConverter.MAX_DIGIT; i++)
+            {
+                for (int j = FlagConverter.MIN_DIGIT; j <= FlagConverter.MAX_DIGIT; j++)
+                {
+                    var v = (10 * i) + j;
+                    if (!h.ContainsKey(v))
+                        h.Add(v, new HashSet<(int, int)>() { (i, j) });
+                    else
+                        h[v].Add((i, j));
+                }
+            }
+
+            foreach (var kvp in h)
+            {
+                var hs = new HashSet<(int, int)>(FlagConverter.Iterate2Flags(kvp.Key));
+                try
+                {
+                    Assert.Equal(kvp.Value, hs);
+                }
+                catch
+                {
+                    output.WriteLine($"FAIL on {kvp.Key}:");
+                    output.WriteLine($"Expected: {string.Join(", ", kvp.Value)}");
+                    output.WriteLine($"Actual: {string.Join(", ", hs)}");
+                    throw;
+                }
+            }
+        }
+
+        [Fact]
+        public void TSC3IteratorRangeOk()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => FlagConverter.Iterate2Flags(FlagConverter.MIN_3_DIGIT - 1).ToArray());
+            Assert.Throws<ArgumentOutOfRangeException>(() => FlagConverter.Iterate2Flags(FlagConverter.MAX_3_DIGIT + 1).ToArray());
+        }
+
+        /// <summary>
+        /// Checks that all individual outputs are valid, and that the correct number of values is encountered.
+        /// </summary>
+        [Fact(Skip = "Takes around 5 seconds to run")]
+        public void TSC3IteratorLooksOk()
+        {
+            const int total = 256 * 256 * 256;
+            int actual = 0;
+            for (int i = FlagConverter.MIN_3_DIGIT; i <= FlagConverter.MAX_3_DIGIT; i++)
+            {
+                foreach (var o in FlagConverter.Iterate3Flags(i))
+                {
+                    actual++;
+                    try
+                    {
+                        Assert.InRange(o.Item1, FlagConverter.MIN_DIGIT, FlagConverter.MAX_DIGIT);
+                        Assert.InRange(o.Item2, FlagConverter.MIN_DIGIT, FlagConverter.MAX_DIGIT);
+                        Assert.InRange(o.Item3, FlagConverter.MIN_DIGIT, FlagConverter.MAX_DIGIT);
+                        Assert.Equal(i, (o.Item1 * 100) + (o.Item2 * 10) + o.Item3);
+                    }
+                    catch
+                    {
+                        output.WriteLine($"{i} => {o} = {(o.Item1 * 100) + (o.Item2 * 10) + o.Item3}");
+                        throw;
+                    }
+                }
+            }
+            Assert.Equal(total, actual);
+        }
+
+        [Fact]
+        public void TSC4IteratorRangeOk()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => FlagConverter.Iterate2Flags(FlagConverter.MIN_4_DIGIT - 1).ToArray());
+            Assert.Throws<ArgumentOutOfRangeException>(() => FlagConverter.Iterate2Flags(FlagConverter.MAX_4_DIGIT + 1).ToArray());
+        }
+        /// <summary>
+        /// Checks that all individual outputs are valid, and that the correct number of values is encountered.
+        /// </summary>
+        //TODO consider using multithreading to reduce runtime?
+        [Fact(Skip = "Takes around 30 minutes to run")]
+        public void TSC4IteratorLooksOk()
+        {
+            //My want to be explicit requires this to be 64 bit
+            const long total = 256L * 256L * 256L * 256L;
+            long actual = 0;
+            for (int i = FlagConverter.MIN_4_DIGIT; i <= FlagConverter.MAX_4_DIGIT; i++)
+            {
+                foreach (var o in FlagConverter.Iterate4Flags(i))
+                {
+                    actual++;
+                    try
+                    {
+                        Assert.InRange(o.Item1, FlagConverter.MIN_DIGIT, FlagConverter.MAX_DIGIT);
+                        Assert.InRange(o.Item2, FlagConverter.MIN_DIGIT, FlagConverter.MAX_DIGIT);
+                        Assert.InRange(o.Item3, FlagConverter.MIN_DIGIT, FlagConverter.MAX_DIGIT);
+                        Assert.InRange(o.Item4, FlagConverter.MIN_DIGIT, FlagConverter.MAX_DIGIT);
+                        Assert.Equal(i, (o.Item1 * 1000) + (o.Item2 * 100) + (o.Item3 * 10) + o.Item4);
+                    }
+                    catch
+                    {
+                        output.WriteLine($"{i} => {o} = {(o.Item1 * 1000) + (o.Item2 * 100) + (o.Item3 * 10) + o.Item4}");
+                        throw;
+                    }
+                }
+            }
+            Assert.Equal(total, actual);
+        }
+    }
+}
